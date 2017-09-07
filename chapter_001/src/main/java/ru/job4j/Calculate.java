120ee5eb36b8069c447ff1b14c1679fe3720fcf2
CyMpak1989 <cympak2009@mail.ru>
2017-09-07 19:33:42 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j;
 
+/**
+ * Class Calculate
+ *
+ * @author CyMpak1989 (cympak2009@mail.ru)
+ * @version 1.0
+ * @since 07.09.2017
+ */
+
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+/**
+ * Class package-info
+ *
+ * @author CyMpak1989 (cympak2009@mail.ru)
+ * @version 1.0
+ * @since 07.09.2017
+ */
