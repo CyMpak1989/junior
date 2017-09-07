be0ea43bcdf0c3dcf7ecdaf130dd5a6a5156c9d0
CyMpak1989 <cympak2009@mail.ru>
2017-09-07 20:01:47 +0300

    task 4122

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class Calculate
+ *
+ * @author CyMpak1989 (cympak2009@mail.ru)
+ * @version 1.0
+ * @since 07.09.2017
+ */
+
+public class CalculateTest {
+@Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Petr Arsentev";
+    String expect = "Echo, echo, echo : Petr Arsentev"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+}
