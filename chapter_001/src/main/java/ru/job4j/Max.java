2acf715e2b3276e7852b804ed25e2effe9494217
CyMpak1989 <cympak2009@mail.ru>
2017-09-11 12:28:38 +0300

    task 189

---
+++ chapter_001/src/main/java/ru/job4j/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+/**
+ * Class Max. Находим максимально число.
+ *
+ * @author CyMpak1989 (cympak2009@mail.ru)
+ * @version 1.0
+ * @since 11.09.2017
+ */
+public class Max {
+    /**
+     * Method max. Принимает на вход 2 параметра на выходе вернет максимально число.
+     *
+     * @param first  первое число
+     * @param second второе число
+     * @return Максимальное число
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
