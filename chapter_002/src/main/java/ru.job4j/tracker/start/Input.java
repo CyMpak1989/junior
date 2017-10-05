982a9fd3a97e218da21eba273fef3cafc8a95bcc
CyMpak1989 <cympak2009@mail.ru>
2017-10-06 00:41:40 +0300

    Задача 784 не готова

--- chapter_002/src/main/java/ru.job4j/tracker/start/Input.java
+++ chapter_002/src/main/java/ru.job4j/tracker/start/Input.java
@@ -2,4 +2,7 @@ package ru.job4j.tracker.start;
 
 public interface Input {
     String ask(String question);
+
+    String getKey();
+
 }
--- chapter_002/src/main/java/ru.job4j/tracker/start/StartUI.java
+++ chapter_002/src/main/java/ru.job4j/tracker/start/StartUI.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker.start;
 
 import ru.job4j.tracker.models.Item;
+import ru.job4j.tracker.models.Task;
 
 /**
  * Class StartUI.
@@ -10,6 +11,7 @@ import ru.job4j.tracker.models.Item;
  * @since 29.09.2017
  */
 public class StartUI {
+    private Input input;
     private static final String ADD_NEW_ITEM = "0";
     private static final String SHOW_ALL_ITEM = "1";
     private static final String EDIT_ITEM = "2";
@@ -26,29 +28,55 @@ public class StartUI {
             "6. Exit Program\n" +
             "Select:";
 
+    public StartUI(Input input) {
+        this.input = input;
+    }
+
     /**
      * Метод для запуска программы.
+     *
      * @param args входящие параметры
      */
     public static void main(String[] args) {
-        ConsoleInput input = new ConsoleInput();
-        while (!EXIT_PROGRAM.equals(input.ask(MENU))){
-            if (ADD_NEW_ITEM.equals(input.getKey())){
+        new StartUI(new ConsoleInput()).init();
+    }
 
-            } else if (SHOW_ALL_ITEM.equals(input.getKey())){
+    public void init() {
+        Tracker tracker = new Tracker();
+        while (!EXIT_PROGRAM.equals(this.input.ask(MENU))) {
+            if (ADD_NEW_ITEM.equals(this.input.getKey())) {
+                addNewItem(this.input, tracker);
+            } else if (SHOW_ALL_ITEM.equals(this.input.getKey())) {
+                showAllItem(tracker);
+            } else if (EDIT_ITEM.equals(this.input.getKey())) {
+                editItem(this.input, tracker);
+            } else if (DELETE_ITEM.equals(this.input.getKey())) {
 
-            } else if (EDIT_ITEM.equals(input.getKey())){
+            } else if (FIND_ITEM_BY_ID.equals(this.input.getKey())) {
 
-            } else if (DELETE_ITEM.equals(input.getKey())){
-
-            } else if (FIND_ITEM_BY_ID.equals(input.getKey())){
-
-            } else if (FIND_ITEM_BY_NAME.equals(input.getKey())){
+            } else if (FIND_ITEM_BY_NAME.equals(this.input.getKey())) {
 
             }
         }
     }
-    public void addNewItem(){
+
+    public void addNewItem(Input input, Tracker tracker) {
+        Item item = new Task(input.ask("Введите имя заявки: "), input.ask("Введите дескприптор: "));
+        tracker.add(item);
+        System.out.println("Новая заявка создана!\n");
+
+    }
+
+    public void showAllItem(Tracker tracker) {
+        Item[] items = tracker.findAll();
+        for (Item item : items) {
+            System.out.println("Id: " + item.getId() + " Name: " + item.getName() +
+                    " Description: " + item.getDescription() + " Create: " + item.getCreate());
+        }
+        System.out.println("Список всех задач на экране!\n");
+    }
+
+    public void editItem(Input input, Tracker tracker){
 
     }
 }
