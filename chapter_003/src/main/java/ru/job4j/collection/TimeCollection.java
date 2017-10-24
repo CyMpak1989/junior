43bf04c63ed71689272d6389367a14b97723a77b
CyMpak1989 <cympak2009@mail.ru>
2017-10-25 00:04:47 +0300

    Задача 10022

--- chapter_003/src/main/java/ru/job4j/collection/TimeCollection.java
+++ chapter_003/src/main/java/ru/job4j/collection/TimeCollection.java
@@ -1,36 +1,75 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeSet;
 
+
+/**
+ * Класс TimeCollection.
+ */
 public class TimeCollection {
-    Date date = new Date();
-    long startTime = 0;
-    long endTime = 0;
+    /**
+     * Хреним начальное время.
+     */
+    private long startTime = 0;
+    /**
+     * Храним время окончания.
+     */
+    private long endTime = 0;
 
+    /**
+     * Метод добаления данных в Collection.
+     * @param collection принимаем реализацию коллекции.
+     * @param amount принимаем количество добалений в коллекцию.
+     * @return вернем время операции.
+     */
     public long add(Collection<String> collection, int amount) {
-        startTime = date.getTime();
+        this.startTime = new Date().getTime();
         for (int i = 0; i < amount; i++) {
             collection.add(i + "Тест.");
         }
-        endTime = date.getTime();
+        this.endTime = new Date().getTime();
         return endTime - startTime;
     }
 
+    /**
+     * Метод удаления данных из Collection.
+     * @param collection принимаем реализацию коллекции.
+     * @param amount принимаем количество добалений в коллекцию.
+     * @return вернем время операции.
+     */
     public long delete(Collection<String> collection, int amount) {
-        startTime = date.getTime();
+        this.startTime = new Date().getTime();
         for (int i = 0; i < amount; i++) {
             collection.remove(i);
         }
-        endTime = date.getTime();
+        this.endTime = new Date().getTime();
         return endTime - startTime;
     }
 
+    /**
+     * Старт программы.
+     * @param args аргуметы.
+     */
     public static void main(String[] args) {
+        //Объект класса TimeCollection.
         TimeCollection timeCollection = new TimeCollection();
+        //Обьекты коллекции.
         Collection<String> stringArrayList = new ArrayList<>();
-        System.out.println(timeCollection.add(stringArrayList, 1000));
-        System.out.println(timeCollection.delete(stringArrayList, 1000));
+        Collection<String> stringLinkedList = new LinkedList<>();
+        Collection<String> stringTreeSet = new TreeSet<>();
+        //Вывод на в консоль.
+        System.out.println("ArrayList");
+        System.out.println(timeCollection.add(stringArrayList, 10000));
+        System.out.println(timeCollection.delete(stringArrayList, 10000));
+        System.out.println("LinkedList");
+        System.out.println(timeCollection.add(stringLinkedList, 10000));
+        System.out.println(timeCollection.delete(stringLinkedList, 10000));
+        System.out.println("TreeSet");
+        System.out.println(timeCollection.add(stringTreeSet, 10000));
+        System.out.println(timeCollection.add(stringTreeSet, 10000));
     }
 }
