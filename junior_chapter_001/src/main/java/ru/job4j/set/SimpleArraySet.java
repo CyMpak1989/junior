a37d4879fb37252bbc9ef8003d2cc0ae2a5b6d3f
CyMpak1989 <cympak2009@mail.ru>
2017-12-27 22:47:29 +0300

    Task 996 SimpleArraySet

---
+++ junior_chapter_001/src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -0,0 +1,46 @@
+package ru.job4j.set;
+
+import ru.job4j.list.DynamicArrayList;
+
+import java.util.Iterator;
+
+public class SimpleArraySet<T> implements Iterable<T> {
+    DynamicArrayList<T> ts;
+
+    public SimpleArraySet() {
+        this.ts = new DynamicArrayList<>();
+    }
+
+    public void add(T e) {
+        if (!checkForDuplicates(e)) {
+            ts.add(e);
+        }
+    }
+
+    public boolean checkForDuplicates(T e) {
+        Iterator<T> iterator = ts.iterator();
+        while (iterator.hasNext()) {
+            if (e == iterator.next()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Iterator<T> iterator = ts.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public T next() {
+                return iterator.next();
+            }
+        };
+    }
+}
