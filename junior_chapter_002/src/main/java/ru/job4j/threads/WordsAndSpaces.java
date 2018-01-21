8018e7c3259decbdae0c8217cfdb075deb0f7dd4
CyMpak1989 <cympak2009@mail.ru>
2018-01-21 20:52:11 +0300

    Task 1017 WordsAndSpaces the pending withdrawal

--- junior_chapter_002/src/main/java/ru/job4j/threads/WordsAndSpaces.java
+++ junior_chapter_002/src/main/java/ru/job4j/threads/WordsAndSpaces.java
@@ -1,36 +1,56 @@
 package ru.job4j.threads;
 
 public class WordsAndSpaces {
-    public void text(String x) {
-        new Thread() {
-            @Override
-            public void run() {
-                new Thread() {
-                    @Override
-                    public void run() {
-                        String[] array = x.split(" ");
-                        System.out.println("Колличество слов в тексте: " + array.length);
-                    }
-                }.start();
-                new Thread() {
-                    @Override
-                    public void run() {
-                        char[] chars = x.toCharArray();
-                        int counts = 0;
-                        for (int i = 0; i < chars.length; i++) {
-                            if (chars[i] == ' ') {
-                                counts++;
-                            }
-                        }
-                        System.out.println("Количсетво пробелов в тексте: " + counts);
-                    }
-                }.start();
-            }
-        }.start();
+    public static class Words extends Thread {
+        String text;
+
+        public Words(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public void run() {
+            String[] array = text.split(" ");
+            System.out.println("Колличество слов в тексте: " + array.length);
+        }
     }
 
+    public static class Spaces extends Thread {
+        String text;
+
+        public Spaces(String text) {
+            this.text = text;
+        }
+
+        public void run() {
+            char[] chars = text.toCharArray();
+            int counts = 0;
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == ' ') {
+                    counts++;
+                }
+            }
+            System.out.println("Количсетво пробелов в тексте: " + counts);
+        }
+    }
+
+
     public static void main(String[] args) {
-        WordsAndSpaces wordsAndSpaces = new WordsAndSpaces();
-        wordsAndSpaces.text("Привет как дела ?");
+        System.out.println("Start programm!!!");
+        Thread thread1 = new Words("Привет как дела ?");
+        thread1.start();
+        Thread thread2 = new Spaces("Привет как дела ?");
+        thread2.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("End programm!!!");
     }
 }
--- junior_chapter_002/src/test/java/ru/job4j/threads/WordsAndSpacesTest.java
+++ junior_chapter_002/src/test/java/ru/job4j/threads/WordsAndSpacesTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 public class WordsAndSpacesTest {
     @Test
     public void text() throws Exception {
-        WordsAndSpaces wordsAndSpaces = new WordsAndSpaces();
-        wordsAndSpaces.text("Привет Как дела ?");
+
     }
 }
