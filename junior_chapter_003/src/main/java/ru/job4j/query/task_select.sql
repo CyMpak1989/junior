07ae31be7a800c4007f5392b55bb04101a14cb06
CyMpak <cympak2009@mail.ru>
2018-02-19 09:39:40 +0300

    fix Task 1732

--- junior_chapter_003/src/main/java/ru/job4j/query/task_select.sql
+++ junior_chapter_003/src/main/java/ru/job4j/query/task_select.sql
@@ -1,5 +1,7 @@
 -- 1. Написать запрос получение всех продуктов с типом "СЫР"
-SELECT * FROM product AS p WHERE p.type_id = '1';
+SELECT product.name, t.name FROM product
+INNER JOIN type t ON product.type_id = t.id
+WHERE t.name = 'Сыр';
 -- 2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
 SELECT * FROM product AS p WHERE p.name LIKE '%Мороженое%';
 -- 3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
@@ -13,4 +15,4 @@ SELECT * FROM product AS p WHERE p.type_id IN (1, 2);
 -- 7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
 SELECT p.name, p.type_id FROM product AS p WHERE p.price < 10;
 -- 8. Вывести все продукты и их тип.
-SELECT p.name, t.name FROM product as p INNER JOIN type as t ON p.type_id = t.id;
+SELECT p.name, t.name FROM product AS p INNER JOIN type as t ON p.type_id = t.id;
