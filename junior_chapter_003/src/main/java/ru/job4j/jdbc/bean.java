53eed164f8386a3505e2f99b9071a3fe339408e2
CyMpak <cympak2009@mail.ru>
2018-03-02 15:23:28 +0300

    Task 20459

---
+++ junior_chapter_003/src/main/java/ru/job4j/jdbc/bean.java
@@ -0,0 +1,13 @@
+package ru.job4j.jdbc;
+
+public class bean {
+    private String field;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
