9c81330da842e28ca37742db832b68f824ae1fe6
CyMpak1989 <cympak2009@mail.ru>
2018-03-04 00:49:23 +0300

    Task 20459

--- junior_chapter_003/src/main/java/ru/job4j/jdbc/SQLStorage.java
+++ junior_chapter_003/src/main/java/ru/job4j/jdbc/SQLStorage.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
-import java.util.Collection;
-import java.util.Properties;
 
 /**
  * @author Vladimir Lembikov (cympak2009@mail.ru) on 03.03.2018.
@@ -13,30 +11,37 @@ import java.util.Properties;
  * @since 0.1.
  */
 public class SQLStorage {
-    private static final Logger Log = LoggerFactory.getLogger(SQLStorage.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SQLStorage.class);
 
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/java_a_from_z";
         String username = "postgres";
-        String password = "";
+        String password = "postgres";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, username, password);
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM type");
+//            PreparedStatement ps = conn.prepareStatement("INSERT INTO type (name) VALUES (?)");
+//            ps.setString(1, "Крахмал");
+//            int rowsDelete = ps.executeUpdate();
+//            ps.close();
+//            PreparedStatement ps = conn.prepareStatement("SELECT * FROM type AS t WHERE t.id IN (?, ?)");
+//            ps.setInt(1, 1);
+//            ps.setInt(2, 2);
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM type");
+            ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 System.out.println(String.format("%s %s", rs.getInt("id"), rs.getString("name")));
             }
             rs.close();
-            st.close();
+            ps.close();
         } catch (SQLException e) {
-            Log.error(e.getMessage(), e);
+            LOG.error(e.getMessage(), e);
         } finally {
             if (conn != null) {
                 try {
                     conn.close();
                 } catch (SQLException e) {
-                    Log.error(e.getMessage(), e);
+                    LOG.error(e.getMessage(), e);
                 }
             }
         }
