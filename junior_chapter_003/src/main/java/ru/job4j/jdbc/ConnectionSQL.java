1116e30ba5e83285be9681088b4ee27db7610408
CyMpak1989 <cympak2009@mail.ru>
2018-03-15 23:16:16 +0300

    Task 20459

--- junior_chapter_003/src/main/java/ru/job4j/jdbc/ConnectionSQL.java
+++ junior_chapter_003/src/main/java/ru/job4j/jdbc/ConnectionSQL.java
@@ -4,18 +4,37 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Класс ConnectionSQL.
+ *
+ * @author Vladimir Lembikov (cympak2009@mail.ru) on 12.03.2018.
+ * @version 1.0.
+ * @since 0.1.
+ */
 public class ConnectionSQL {
     private String url;
     private String username;
     private String password;
     private Connection connection;
 
+    /**
+     * Конструктор.
+     *
+     * @param url      адрес подключения к БД.
+     * @param username логин для подключения к БД.
+     * @param password пароль для пдключения к БД.
+     */
     public ConnectionSQL(String url, String username, String password) {
         this.url = url;
         this.username = username;
         this.password = password;
     }
 
+    /**
+     * Геттер для получения доступа к БД.
+     *
+     * @return вернем ссылку на новое подключения к БД.
+     */
     public Connection getConnection() {
         try {
             connection = DriverManager.getConnection(url, username, password);
@@ -27,9 +46,13 @@ public class ConnectionSQL {
         return connection;
     }
 
+    /**
+     * Метод для закрытия доступа к БД.
+     */
     public void closeConnection() {
         try {
             connection.close();
+            System.out.println("Соединение потеряно.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
