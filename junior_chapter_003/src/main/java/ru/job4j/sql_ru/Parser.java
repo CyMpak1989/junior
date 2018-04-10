de6b5030f4b3706ea3528b4a8ce4c984b7acdcd8
CyMpak <cympak2009@mail.ru>
2018-04-10 16:01:03 +0300

    Task 1731

--- junior_chapter_003/src/main/java/ru/job4j/sql_ru/Parser.java
+++ junior_chapter_003/src/main/java/ru/job4j/sql_ru/Parser.java
@@ -6,19 +6,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Parser {
     public static void main(String[] args) {
         Document document;
         try {
-            document = Jsoup.connect("http://www.sql.ru/forum/job-offers/").get();
+            document = Jsoup.connect("http://www.sql.ru/forum/job-offers/1").get();
             Elements elements = document.getElementsByAttributeValue("class", "postslisttopic");
             //Elements elements = document.getElementsByTag("tr");
             System.out.println(elements.size());
             for (Element element : elements) {
                 String url = element.getElementsByTag("a").attr("href");
                 String text = element.getElementsByTag("a").eachText().get(0);
-                System.out.println(url + " " + text);
+                if (Pattern.compile("[j,J]ava\\s?(?=SE/EE|SE|EE)?(?!\\s?[s,S]cript)").matcher(text).find()) {
+                    System.out.println(url + " " + text);
+                }
+
             }
 
         } catch (IOException e) {
