e537126116520d5151b1614a7e425d55330f1ade
CyMpak <cympak2009@mail.ru>
2018-06-13 15:02:11 +0300

    Приступил к повторному прохождению Task 2512

--- junior_chapter_004/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ junior_chapter_004/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -3,7 +3,6 @@ package ru.job4j.servlets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,10 +18,10 @@ public class EchoServlet extends HttpServlet {
     private static final Logger LOG = LoggerFactory.getLogger(EchoServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("Hello world!!!");
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        writer.append("hello world");
         writer.flush();
     }
 }
