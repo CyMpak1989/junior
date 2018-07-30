de8741f7be790d94c7a3736a7b92513694f8a805
CyMpak1989 <cympak2009@mail.ru>
2018-07-31 00:10:40 +0300

    Task 2518

---
+++ junior_chapter_004/src/test/java/ru/job4j/servlets/UserUpdateServletTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.servlets;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Vladimir Lembikov (cympak2009@mail.ru) on 31.07.2018.
+ * @version 1.0.
+ * @since 0.1.
+ */
+public class UserUpdateServletTest {
+    private static final Logger LOG = LoggerFactory.getLogger(UserUpdateServletTest.class);
+
+    @Test
+    public void updateUser() {
+
+    }
+}
