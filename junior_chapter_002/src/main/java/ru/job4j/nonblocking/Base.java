29796d538aec46c7a0ace7299f5ca3ecfa3a8779
CyMpak <cympak2009@mail.ru>
2018-08-30 15:54:26 +0300

    task 4741

---
+++ junior_chapter_002/src/main/java/ru/job4j/nonblocking/Base.java
@@ -0,0 +1,22 @@
+package ru.job4j.nonblocking;
+
+public class Base {
+    private int id;
+    private int version;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+}
---
+++ junior_chapter_002/src/main/java/ru/job4j/nonblocking/NonBlockCache.java
@@ -0,0 +1,26 @@
+package ru.job4j.nonblocking;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class NonBlockCache {
+    private ConcurrentHashMap<Integer, Base> cache = new ConcurrentHashMap<>();
+
+    public void add(Base model) {
+        cache.put(model.getId(), model);
+    }
+
+    public void update(Base model) {
+        cache.computeIfPresent(model.getId(), (k, v) -> {
+            if (cache.get(k).getVersion() == model.getVersion()) {
+                model.setVersion(model.getVersion() + 1);
+                return model;
+            } else {
+                throw new OptimisticException("Model already update");
+            }
+        });
+    }
+
+    public void delete(Base model) {
+
+    }
+}
