b670ba7c9d503d32996be3070bd4ce7788a49a96
CyMpak1989 <cympak2009@mail.ru>
2018-08-31 00:15:10 +0300

    task 4741

--- junior_chapter_002/src/test/java/ru/job4j/nonblocking/NonBlockCacheTest.java
+++ junior_chapter_002/src/test/java/ru/job4j/nonblocking/NonBlockCacheTest.java
@@ -1,9 +1,12 @@
 package ru.job4j.nonblocking;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class NonBlockCacheTest {
     @Test
@@ -12,7 +15,7 @@ public class NonBlockCacheTest {
         Thread thread = new Thread(
                 () -> {
                     try {
-                        throw new RuntimeException("Throw Exception in Thread");
+                        throw new OptimisticException("Throw Exception in Thread");
                     } catch (Exception e) {
                         ex.set(e);
                     }
@@ -20,6 +23,51 @@ public class NonBlockCacheTest {
         );
         thread.start();
         thread.join();
-        Assert.assertThat(ex.get().getMessage(), is("Throw Exception in Thread"));
+        assertThat(ex.get().getMessage(), is("Throw Exception in Thread"));
+    }
+
+    @Test
+    public void whenExpectExceptionThenException() throws Exception {
+        Base base = new Base();
+        base.setId(1);
+        base.setVersion(1);
+        NonBlockCache cache = new NonBlockCache();
+        cache.add(base);
+        AtomicBoolean check = new AtomicBoolean(false);
+
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    cache.update(base);
+                    System.out.println();
+                } catch (OptimisticException e) {
+                    check.set(true);
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            Base base1 = new Base();
+            base1.setId(1);
+            base1.setVersion(5);
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                cache.update(base1);
+            } catch (OptimisticException e) {
+                check.set(true);
+            }
+        }).start();
+
+        Thread.sleep(10);
+
+        assertThat(check.get(), is(true));
     }
 }
