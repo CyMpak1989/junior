6c3dc739bb479b1f5622301bd8f4585163b209b6
CyMpak <cympak2009@mail.ru>
2018-08-31 12:23:39 +0300

    merge

--- junior_chapter_002/src/test/java/ru/job4j/nonblocking/NonBlockCacheTest.java
+++ junior_chapter_002/src/test/java/ru/job4j/nonblocking/NonBlockCacheTest.java
@@ -3,9 +3,12 @@ package ru.job4j.nonblocking;
 import org.junit.Test;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import java.util.concurrent.atomic.AtomicBoolean;
 >>>>>>> b670ba7... task 4741
+=======
+>>>>>>> a4c54db... task 4741
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.hamcrest.core.Is.is;
@@ -13,6 +16,7 @@ import static org.junit.Assert.assertThat;
 
 public class NonBlockCacheTest {
     @Test
+<<<<<<< HEAD
 <<<<<<< HEAD
     public void whenWeExpectAnException() throws Exception {
         Base base = new Base();
@@ -68,47 +72,48 @@ public class NonBlockCacheTest {
 
     @Test
     public void whenExpectExceptionThenException() throws Exception {
+=======
+    public void whenWeExpectAnException() throws Exception {
+>>>>>>> a4c54db... task 4741
         Base base = new Base();
         base.setId(1);
         base.setVersion(1);
         NonBlockCache cache = new NonBlockCache();
         cache.add(base);
-        AtomicBoolean check = new AtomicBoolean(false);
+        AtomicReference<Exception> check = new AtomicReference();
 
-        new Thread(() -> {
+        Thread thread = new Thread(() -> {
             for (int i = 0; i < 5; i++) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 try {
                     cache.update(base);
                     System.out.println();
                 } catch (OptimisticException e) {
-                    check.set(true);
+                    check.set(e);
                 }
             }
-        }).start();
-        new Thread(() -> {
+        });
+
+        Thread thread1 = new Thread(() -> {
             Base base1 = new Base();
             base1.setId(1);
             base1.setVersion(5);
             try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
                 cache.update(base1);
             } catch (OptimisticException e) {
-                check.set(true);
+                check.set(e);
             }
-        }).start();
+        });
 
-        Thread.sleep(10);
+        thread.start();
+        thread1.start();
+        thread.join();
+        thread1.join();
 
+<<<<<<< HEAD
         assertThat(check.get(), is(true));
 >>>>>>> b670ba7... task 4741
+=======
+        assertThat(check.get().getMessage(), is("Model already update"));
+>>>>>>> a4c54db... task 4741
     }
 }
